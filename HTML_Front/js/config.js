968d68ae295866f7a6e8144798ede82799e39489
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2025-12-29 18:28:21 +0100

    Update config.js Ref #75

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,5 +1,5 @@
-const COGNITO_DOMAIN = "https://us-east-1sorxmt05c.auth.us-east-1.amazoncognito.com"; // AWS-Cognito Domain
+const COGNITO_DOMAIN = "https://eu-north-13b5yu2rcv.auth.eu-north-1.amazoncognito.com"; // AWS-Cognito Domain
 
-const COGNITO_CLIENT_ID = "1ag2000lnpg0uj1i9em4uflfcu"; // AWS-Cognito Client-Id
+const COGNITO_CLIENT_ID = "2mpljb6qtrof7qbsa05e3qn1g8"; // AWS-Cognito Client-Id
 
 const REDIRECT_URI = "https://d84l1y8p4kdic.cloudfront.net"; // Redirect Url (wohin soll nach login geleitet werden)
