5ff868d0ad120d126d5d5666b22487f88993bd5f
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2025-12-30 14:55:06 +0100

    Update home.js Ref #81
    Event für register hinzugefügt

--- HTML_Front/js/home.js
+++ HTML_Front/js/home.js
@@ -1,12 +1,19 @@
-import { login, logout } from "./auth.js";
+import { login, logout, register } from "./auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const loginBtn = document.getElementById("btnLogin");
+    const registBtn = document.getElementById("btnRegister")
 
     if (loginBtn) {
         loginBtn.addEventListener("click", () => {
             login();
+        });
+    }
+
+    if (registBtn) {
+        registBtn.addEventListener("click", () => {
+            register();
         });
     }
 
