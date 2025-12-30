3902404155a0b665cdace0ef15dae961d2466f13
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2025-12-30 14:09:31 +0100

    Update config.js Ref #81

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,8 +1,8 @@
 export const cognitoConfig = {
     authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_3B5YU2rcV",
     clientId: "2mpljb6qtrof7qbsa05e3qn1g8",
-    redirectUri: "https://amplify.d3422wv23xn9dj.amplifyapp.com/callback.html",
-    logoutUri: "https://amplify.d3422wv23xn9dj.amplifyapp.com/home.html",
+    redirectUri: "https://amplify.d1v1bk12uazf00.amplifyapp.com/callback.html",
+    logoutUri: "https://amplify.d1v1bk12uazf00.amplifyapp.com",
     cognitoDomain: "https://eu-north-135byu2rcv.auth.eu-north-1.amazoncognito.com",
     scope: "openid email profile"
 };
