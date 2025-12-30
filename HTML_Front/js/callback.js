9974aec7548bfd3f8b9d55fb7e03aad72b7a7b9b
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2025-12-30 13:55:28 +0100

    Update callback.js. Ref #81

--- HTML_Front/js/callback.js
+++ HTML_Front/js/callback.js
@@ -1,28 +1,30 @@
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code");
+import { userManager } from "./auth.js";
+
 const statusText = document.getElementById("statusText");
 
-if (!code) {
-    statusText.innerHTML = `
-        <span class="error">
-            Login fehlgeschlagen.<br>
-            Ungültige oder fehlende Authentifizierung.<br><br>
-            Du wirst in 10 Sekunden zurückgeleitet…
-        </span>
-    `;
-
-    setTimeout(() => {
-        window.location.href = "home.html";
-    }, 10000);
-
-} else {
-    statusText.innerHTML = `
-        <span class="success">
-            Login erfolgreich.<br>
-            Weiterleitung…
-        </span>
-    `;
-    setTimeout(() => {
-        window.location.href = "home.html";
-    }, 1500);
-}
+userManager.signinRedirectCallback()
+    .then(user => {
+        statusText.innerHTML = `
+            <span class="success">
+                Login erfolgreich.<br>
+                Willkommen, ${user.profile.email || "Benutzer"}!<br>
+                Weiterleitung…
+            </span>
+        `;
+        setTimeout(() => {
+            window.location.href = "home.html";
+        }, 1500);
+    })
+    .catch(err => {
+        console.error("Fehler beim Login:", err);
+        statusText.innerHTML = `
+            <span class="error">
+                Login fehlgeschlagen.<br>
+                ${err.message}<br><br>
+                Du wirst in 10 Sekunden zurückgeleitet…
+            </span>
+        `;
+        setTimeout(() => {
+            window.location.href = "home.html";
+        }, 10000);
+    });
