181a907e9f9a5a41963efd1f9df3e8eae8fd6dd8
DieErbse2 <benedikterb04@gmail.com>
2026-01-06 14:18:24 +0100

    showProducts.js angepasst (Automatisches laden von Snacks aus der Datenbank). Ref #145

---
+++ HTML_Front/js/showProducts.js
@@ -0,0 +1,24 @@
+import { orderSnack } from "./order.js";
+const PRODUCTS_URL = "https://h6a15phvcf.execute-api.eu-north-1.amazonaws.com/products";
+document.addEventListener("DOMContentLoaded", loadProducts);
+
+export async function loadProducts() {
+    const res = await fetch(PRODUCTS_URL, { cache: "no-store" });
+    const products = await res.json();
+
+    const container = document.getElementById("products");
+    container.innerHTML = "";
+
+    products.forEach(p => {
+        const btn = document.createElement("button");
+        btn.className = "Snack-button";
+        btn.innerHTML = `
+            <img src=p.image>
+            <span>${p.name}</span>
+            <p>${p.preis} €</p>
+        `;
+
+        btn.addEventListener("click", () => orderSnack(p.produkt_id));
+        container.appendChild(btn);
+    });
+}
