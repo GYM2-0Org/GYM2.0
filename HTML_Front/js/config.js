8cb4f937db1ca5635679e0ed8fca082dc11de257
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2026-01-07 14:28:10 +0100

    Authority abgeändert. Ref #147

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,5 +1,5 @@
 export const cognitoConfig = {
-    authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_anlv1kQRJ",
+    authority: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
     clientId: "64fqsf4bha6al02hpav3pu53vl",
     redirectUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
     logoutUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com",
