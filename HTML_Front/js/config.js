1a0c9d4027482126bc2a0802ecd53c02b460de9b
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2026-01-07 14:57:30 +0100

    Anpassung cognitoConfig. Ref #147

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,9 +1,8 @@
 export const cognitoConfig = {
-    authority: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
-    clientId: "64fqsf4bha6al02hpav3pu53vl",
-    redirectUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
-    logoutUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com",
-    cognitoDomain: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
-    scope: "openid email profile"
+    authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_anlv1kQRJ",
+    client_id: "64fqsf4bha6al02hpav3pu53vl",
+    redirect_uri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
+    response_type: "code",
+    scope: "email openid profile"
 };
 
