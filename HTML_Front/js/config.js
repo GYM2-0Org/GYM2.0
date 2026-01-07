f288892c6c47e7aaad81b2779c25b36d9edeccf4
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2026-01-07 14:43:30 +0100

    cognitoDomain wierder hinzugefügt. Ref #147

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -3,6 +3,7 @@ export const cognitoConfig = {
     clientId: "64fqsf4bha6al02hpav3pu53vl",
     redirectUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
     logoutUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com",
+    cognitoDomain: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
     scope: "openid email profile"
 };
 
