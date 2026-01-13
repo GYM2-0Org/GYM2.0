709a409b1faa94790dc0c65d3a5eddef31930bd1
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2026-01-13 10:44:40 +0100

    Update config.js. Ref #153
    Ergänzung von CognitoDomain und logoutUri

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,8 +1,10 @@
 export const cognitoConfig = {
     Region: "eu-north-1",
+    cognitoDomain: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
     authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_anlv1kQRJ",
     client_id: "64fqsf4bha6al02hpav3pu53vl",
     redirect_uri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
+    logoutUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com",
     response_type: "code",
     scope: "email openid profile"
 };
