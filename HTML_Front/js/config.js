7b3c91745ea5c77ae0552eb95fea9ff9c17cc29b
DieErbse2 <benedikterb04@gmail.com>
2026-01-19 16:48:46 +0100

    change config.js. Ref #226

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,7 +1,7 @@
 export const cognitoConfig = {
     Region: "eu-north-1",
     cognitoDomain: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
-    authority: `https://cognito-idp.eu-north-1.amazonaws.com/${window._env_?.COGNITO_USER_POOL_ID}`,
+    authority: "https://eu-north-1anlv1kqrj.auth.eu-north-1.amazoncognito.com",
     client_id: window._env_.COGNITO_CLIENT_ID,
     redirect_uri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
     logoutUri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com",
